4d360d0f60e6b9ade6611419998f16e7ed36ea38
Muhamad Dodi Fahri RZN <134904025+Fahrizaness@users.noreply.github.com>
2025-07-03 15:13:48 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,20 +1,24 @@
 <?php
 class Database {
-    private $host = 'localhost';
-    private $db_name = 'tumbuh1percent';
-    private $username = 'root';
-    private $password = '';
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
     private $conn;
+
+    public function __construct() {
+        $this->host     = getenv('MYSQLHOST') ?: 'localhost';
+        $this->db_name  = getenv('MYSQLDATABASE') ?: 'tumbuh1percent';
+        $this->username = getenv('MYSQLUSER') ?: 'root';
+        $this->password = getenv('MYSQLPASSWORD') ?: '';
+    }
 
     public function connect() {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO(
-                'mysql:host=' . $this->host . ';dbname=' . $this->db_name,
-                $this->username,
-                $this->password
-            );
+            $dsn = "mysql:host={$this->host};dbname={$this->db_name};charset=utf8mb4";
+            $this->conn = new PDO($dsn, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch(PDOException $e) {
             echo 'Connection Error: ' . $e->getMessage();
